45e13772b28e9a3fcde841d52d9b6f4e3eb5ac65
Galindo Alvarado Masao Yael <72528648+Masao28@users.noreply.github.com>
2022-07-26 22:46:19 -0500

    excelen

--- app/Exports/CitasExport.php
+++ app/Exports/CitasExport.php
@@ -4,8 +4,11 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use App\Models\Citas;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class CitasExport implements FromCollection
+
+class CitasExport implements FromCollection, WithHeadings
+
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +17,17 @@ class CitasExport implements FromCollection
     {
         return Citas::all();
 
+    }
+    public function headings(): array
+    {
+        return [
+            'Id',
+            'Hora_cita',
+            'cita',
+            'id_usuario',
+            'asunto',
+            '',
+            'Fecha'
+        ];
     }
 }
